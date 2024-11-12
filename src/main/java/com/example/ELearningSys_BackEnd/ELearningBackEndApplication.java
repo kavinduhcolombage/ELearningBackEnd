2df370032e2e18c7bfdcb10d92c8deed9dd9c4e8
gitkavindu <hansana2024@gmail.com>
2024-11-12 13:29:20 +0530

    initialize project

---
+++ src/main/java/com/example/ELearningSys_BackEnd/ELearningBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.ELearningSys_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ELearningSys_BackEnd/ELearningBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ELearningSys_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
