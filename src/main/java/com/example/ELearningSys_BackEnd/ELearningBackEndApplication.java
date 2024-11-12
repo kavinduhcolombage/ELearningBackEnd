92f4ebcebd14a57635ed81366b0648e814ee9b55
gitkavindu <hansana2024@gmail.com>
2024-11-12 13:40:22 +0530

    add dependency

--- src/main/java/com/example/ELearningSys_BackEnd/ELearningBackEndApplication.java
+++ src/main/java/com/example/ELearningSys_BackEnd/ELearningBackEndApplication.java
@@ -2,12 +2,20 @@ package com.example.ELearningSys_BackEnd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ELearningBackEndApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ELearningBackEndApplication.class, args);
+	}
+
+	@RequestMapping
+	public String home(){
+		return "kavindu";
 	}
 
 }
