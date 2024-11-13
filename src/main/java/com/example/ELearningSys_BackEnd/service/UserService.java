ddbfe69eae3cdfae0dc1a07a295f04849e07e8f1
gitkavindu <hansana2024@gmail.com>
2024-11-13 14:53:47 +0530

    add methods in userservice class

--- src/main/java/com/example/ELearningSys_BackEnd/service/UserService.java
+++ src/main/java/com/example/ELearningSys_BackEnd/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.ELearningSys_BackEnd.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,40 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    // Get a user by ID
+    public Optional<User> getUserById(int id){
+        return userRepository.findById(id);
+    }
+
+    //Create user
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
     
-    
+    //update user
+    public User updateUser(User user){
+
+        Optional<User> existingUserOptional = userRepository.findById(user.getId());
+        
+        if (existingUserOptional.isPresent()) {
+            User existingUser = existingUserOptional.get();
+            existingUser.setUserName(user.getUserName());
+            existingUser.setPassWord(user.getPassWord());
+            existingUser.setEmail(user.getEmail());
+            existingUser.setRole(user.getRole());
+            //save updated entity
+            return userRepository.save(existingUser);
+        }else{
+            return null;
+        }   
+    }
+
+    //delete user by ID
+    public void deleteUser(int id){
+        userRepository.deleteById(id);
+    }
 }
