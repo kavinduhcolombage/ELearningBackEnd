a415f0818059a5b4c4f45e1a3455a41391c440bb
gitkavindu <hansana2024@gmail.com>
2024-11-13 15:17:16 +0530

    add custome method for Jpa

--- src/main/java/com/example/ELearningSys_BackEnd/repository/UserRepository.java
+++ src/main/java/com/example/ELearningSys_BackEnd/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.ELearningSys_BackEnd.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.ELearningSys_BackEnd.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
 
+    //Crete custom Jpa method using naming convention
+    Optional<User> findByUserName(String userName);
 }
