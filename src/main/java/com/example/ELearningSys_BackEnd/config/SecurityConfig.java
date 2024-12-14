80fcd7b374cb82767802db7bae701564961b7d5f
gitkavindu <hansana2024@gmail.com>
2024-12-14 18:21:14 +0530

    implement security filter chain

--- src/main/java/com/example/ELearningSys_BackEnd/config/SecurityConfig.java
+++ src/main/java/com/example/ELearningSys_BackEnd/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.ELearningSys_BackEnd.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,7 +14,29 @@ public class SecurityConfig {
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http.build();
+
+        return http
+            .csrf(customizer -> customizer.disable())
+            .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+            .httpBasic(Customizer.withDefaults()) //for rest api
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .build();
+
+
+        //http.formLogin(Customizer.withDefaults())
+        
+        //behind the happen in http.csrf(customizer -> customizer.disable()); are following as a comment
+        // Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>(){
+
+        //     @Override
+        //     public void customize(CsrfConfigurer<HttpSecurity> customizer) {
+        //         customizer.disable();              
+        //     }
+            
+        // };
+        
+        // http.csrf(custCsrf);
+
     }
     
 }
