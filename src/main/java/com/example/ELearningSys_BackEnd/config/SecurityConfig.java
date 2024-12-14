c6f4ad1d111adbfdfdfd1e61abcac625d4319ca4
gitkavindu <hansana2024@gmail.com>
2024-12-14 15:08:07 +0530

    implement security config(without going default flow)

---
+++ src/main/java/com/example/ELearningSys_BackEnd/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.example.ELearningSys_BackEnd.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configurable
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    
+}
