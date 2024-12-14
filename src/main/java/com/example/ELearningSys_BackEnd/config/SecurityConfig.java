b242082473e5030ae502c787ca3c5d9006a92957
gitkavindu <hansana2024@gmail.com>
2024-12-14 15:14:02 +0530

    bypassing all security

--- src/main/java/com/example/ELearningSys_BackEnd/config/SecurityConfig.java
+++ src/main/java/com/example/ELearningSys_BackEnd/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.example.ELearningSys_BackEnd.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configurable
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.build();
+    }
     
 }
