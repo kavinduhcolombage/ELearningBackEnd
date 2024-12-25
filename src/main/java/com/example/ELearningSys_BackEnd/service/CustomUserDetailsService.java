0ba5f27682b1d87279c58952def38b319cb2e7ef
gitkavindu <hansana2024@gmail.com>
2024-12-25 16:04:02 +0530

    implement CustomUserDetailsService

--- src/main/java/com/example/ELearningSys_BackEnd/service/CustomUserDetailsService.java
+++ src/main/java/com/example/ELearningSys_BackEnd/service/CustomUserDetailsService.java
@@ -1,15 +1,32 @@
 package com.example.ELearningSys_BackEnd.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.example.ELearningSys_BackEnd.model.User;
+import com.example.ELearningSys_BackEnd.model.UserPriciple;
+import com.example.ELearningSys_BackEnd.repository.UserRepository;
+
+@Service
 public class CustomUserDetailsService implements UserDetailsService{
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+        User user = userRepository.findByUsername(username);
+
+        if(user == null){
+            System.out.println("user not found");
+            throw new UsernameNotFoundException("user not found");
+        }
+        // need to return user details, for that userpriciple class created
+        return new UserPriciple(user);
+        
     }
 
 }
